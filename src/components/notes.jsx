fe206e5662fb1fba3f070c99c2e7f58831e5b205
fdAssis <fd07ass@gmail.com>
2021-04-09 11:37:34 -0300

    refactoring component <Notes>

--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -7,9 +7,19 @@ class Notes extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <CardNote />
-        </li>
+        {Array.of('Trabalho', 'Estudo', 'Trabalho', 'Estudo').map(
+          (category) => {
+            return (
+              <li>
+                <header>
+                  <h3>{category}</h3>
+                  <p>20/03/2021</p>
+                </header>
+                <CardNote />
+              </li>
+            );
+          }
+        )}
       </ul>
     );
   }
